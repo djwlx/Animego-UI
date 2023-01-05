4173d1eecbb89948cbaafd4d3d6dc54be773b259
孟笑晨 <1562494673@qq.com>
2023-01-05 18:18:42 +0800

    xiugai

--- src/component/DefaultLayout/index.tsx
+++ src/component/DefaultLayout/index.tsx
@@ -7,6 +7,8 @@ import { Outlet } from "react-router-dom";
 
 const ReactComponent: FC = () => {
   const { Header, Sider, Content } = Layout;
+  
+  console.log(ReactDom.findDOMNode(this.refs.tip))
 
   return (
     <Layout className={style.layout}>
@@ -15,7 +17,10 @@ const ReactComponent: FC = () => {
       </Header>
       <Layout style={{ marginTop: 60 }}>
         <Sider>
-          <SideBar />
+          <div style={{ display: "flex" }}>
+			<div style={{ width: "240px", overflow: "hidden", flex: "0 0 240px", maxWidth: "240px", minWidth: "240px", transition: "background-color 0.3s ease 0s, min-width 0.3s ease 0s, max-width 0.3s cubic-bezier(0.645, 0.045, 0.355, 1) 0s" }}></div>
+			<SideBar />
+          </div>
         </Sider>
         <Content style={{ padding: 10 }}>
           <Outlet />
