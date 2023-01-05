b51e2ce16234a1ee4f76954dd2edd4aa7d7101bb
huobendong <huobendong@bytedance.com>
2023-01-05 10:33:21 +0800

    feat: 去掉多余逻辑,重写样式

--- src/component/DefaultLayout/Header.tsx
+++ src/component/DefaultLayout/Header.tsx
@@ -6,25 +6,6 @@ const Header: FC = () => {
     <div style={{ width: "100%", position: "fixed" }}>
       <Nav
         mode={"horizontal"}
-        items={
-          [
-            // { itemKey: "user", text: "个人中心", icon: <IconUser /> },
-            // { itemKey: "union", text: "活动管理", icon: <IconStar /> },
-            // {
-            //   itemKey: "approve-management",
-            //   text: "审批管理",
-            //   icon: <IconEdit />,
-            //   items: [
-            //     "入驻审核",
-            //     {
-            //       itemKey: "operation-management",
-            //       text: "运营管理",
-            //       items: ["人员管理", "人员变更"],
-            //     },
-            //   ],
-            // },
-          ]
-        }
         onSelect={(key) => console.log(key)}
         header={{
           logo: (
@@ -32,22 +13,6 @@ const Header: FC = () => {
           ),
           text: "个人中心",
         }}
-        footer={
-          <Dropdown
-            position="bottomRight"
-            render={
-              <Dropdown.Menu>
-                <Dropdown.Item>详情</Dropdown.Item>
-                <Dropdown.Item>退出</Dropdown.Item>
-              </Dropdown.Menu>
-            }
-          >
-            <Avatar size="small" color="light-blue" style={{ margin: 4 }}>
-              user
-            </Avatar>
-            <span>用户</span>
-          </Dropdown>
-        }
       />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const ReactComponent: FC = () => {
   const navigate = useNavigate();
   return (
-    <div style={{ height: 2000, backgroundColor: "red" }}>
+    <div style={{ height: 2000 }}>
       hello
       <button onClick={() => navigate("/second")}>跳转</button>
     </div>
