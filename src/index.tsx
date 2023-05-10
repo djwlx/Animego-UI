ebd6081ec7cbba6cebe51b0a2f7252db32a586d8
DJWL <1602951127@qq.com>
2023-05-10 11:37:04 +0800

    feat: 使用vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./layout";
 
-const root = document.getElementById("root");
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
